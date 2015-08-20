558a3d80f3a0dbf597dc778a23b5c5d75099a2dc
Dominic Waterson <dominic.waterson@tombola.com>
2015-08-20 10:22:21 +0100

    Add code to solve pulling information from csv record

--- 1-strings.js
+++ 1-strings.js
@@ -1,3 +1,8 @@
 var helloString = 'Hello World!';
-
 console.log(helloString);
+
+// Extract the records from the follwing CSV record:1997,Ford,E350,"Super luxurious” truck
+var csvRecord = [1997, "Ford", "E350", "Super luxurious truck"];
+for(var x in csvRecord) {
+    console.log(csvRecord[x]);
+}
