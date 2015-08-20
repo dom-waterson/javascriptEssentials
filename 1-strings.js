dc0395b2bdc4aef6bb70a7dcbfaf9a2f3b50f67d
Dominic Waterson <dominic.waterson@tombola.com>
2015-08-20 10:42:20 +0100

    added code to find first vowel in a string using regex

--- 1-strings.js
+++ 1-strings.js
@@ -5,4 +5,10 @@ console.log(helloString);
 var csvRecord = [1997, "Ford", "E350", "Super luxurious truck"];
 for(var x in csvRecord) {
     console.log(csvRecord[x]);
-}
+};
+
+//Find the position of the first vowel in the textJSDFHDVHUEVDAKNDSEINX
+var text = "JSDFHDVHUEVDAKNDSEINX";
+var vowelPostion = text.search(/[aeiouAEIOU]/);
+var letter = text[vowelPostion];
+console.log("The first vowel in " + text + " is: " + letter + ". At postion: " + vowelPostion);
