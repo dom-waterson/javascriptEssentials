9cbaff21a44e51121a81c26a5b7967d6f2b40405
Dominic Waterson <dominic.waterson@tombola.com>
2015-08-21 14:26:32 +0100

    Added task 3 - Grading system

--- 3-comparison.js
+++ 3-comparison.js
@@ -46,3 +46,36 @@ var isNegative = function (product) {
 };
 
 getTheProduct(firstNumber,secondNumber,thirdNumber);
+
+// Task 3 - A grading system: if the score provided is less that 40 = F,
+// less than 50= E etc up to A. No A*,
+// they’re just a whack idea to cover up the fact exams are too easy
+
+var score = 81;
+
+var checkScore = function (score) {
+    if (score >= 80) {
+        printScore("A");
+    }
+    else if (score < 80 && score >= 70) {
+        printScore("B")
+    }
+    else if (score < 70 && score >= 60) {
+        printScore("C");
+    }
+    else if (score < 60 && score >= 50) {
+        printScore("D");
+    }
+    else if (score < 50 && score >= 40) {
+        printScore("E");
+    }
+    else{
+        printScore("F");
+    }
+};
+
+var printScore = function (grade) {
+    console.log("Your score is equal to: " + grade);
+};
+
+checkScore(score);
