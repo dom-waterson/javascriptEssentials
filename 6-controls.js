5dc4500a54c89dd23bdff0c9ee079583fedaa4b8
Dominic Waterson <dominic.waterson@tombola.com>
2015-08-24 11:47:39 +0100

    Add code for fibonacci sequence

--- 6-controls.js
+++ 6-controls.js
@@ -3,7 +3,37 @@ var star = "";
 var printTriangle = function (rows) {
     for (var i = 0; i < rows; i++) {
         console.log(star += "*");
+        for (var i = 0; i <= rows; i++) {
+            console.log(star += "*");
+        }
     }
 }
 
-printTriangle(3);
+//printTriangle(3);
+
+
+
+//Output the Fibonacci sequence up, up to a certain value.
+var printFibonacciSequence = function(endNumber) {
+    var a = 0, b = 1, f = 1;
+    var fibArray = [a, b];
+    do {
+        f = a + b;
+        fibArray.push(f);
+        a = b;
+        b = f;
+    } while (foundNumber(f, endNumber));
+    console.log(fibArray);
+};
+
+
+var foundNumber = function (fibonacciNumber, endNumber) {
+    if (fibonacciNumber === endNumber) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+printFibonacciSequence(21);
+
