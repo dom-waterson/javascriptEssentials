d242cb8e86e8e350efad6d2eb8787e9973a186c0
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-03 10:14:46 +0100

    refactored task2 using less code but still doing the same job

--- 5-arrays.js
+++ 5-arrays.js
@@ -1,24 +1,15 @@
 //Task1 - Output the last element in an array
-var fooArray = ["Hello", 10, "World", 20];
+var fooArray = ["Hello", "dom", "World", "smith", "dom"];
 
 console.log(fooArray.pop());
 
 //Task 2 - Provide a function which outputs the first element of a passed array,
-// or if an optional second value is passed (‘n’ – a number) the first 0­n values
-//TODO: When optionalNumber is passed, the array returning is not correct.
-var getElementInArray = function (array, optionalNumber) {
-    if (optionalNumber === undefined) {
-        console.log(array[0]);
-    }
-    else {
-        var baz = array.slice(0, optionalNumber);
-        for (var i =0; i<baz.length;i++) {
-            console.log(array[i]);
-        }
-    }
+// or if an optional second value is passed (‘n’ – a number) the first 0-­n values
+var getElementInArray = function (array, optionalNumber ) {
+    return array.slice(0,  optionalNumber || 1);
 };
 
-getElementInArray(fooArray,0);
+console.log(getElementInArray(fooArray, 2));
 
 //Converts an array into CSV format
 var arrayToConvert = ["Hello", "World", 50, 60];
