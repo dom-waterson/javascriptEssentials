67232b4bdbeaae805a3ad3d370f4b0a79718c813
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-04 10:28:50 +0100

    added double linked list, need to refactor to reduce the duplicated code

--- 7-objects.js
+++ 7-objects.js
@@ -7,6 +7,7 @@ var Recipe = {
 function Node (data) {
     this.data = data;
     this.next = null;
+    this.previous = null;
 };
 
 function SingleLinkedList () {
@@ -53,13 +54,71 @@ function SingleLinkedList () {
     }
 };
 
-var singleList = new SingleLinkedList();
-singleList.insert("hello", "head");
-singleList.insert("world", "hello");
-singleList.insert("dom", "world");
-singleList.display();
-singleList.remove("world");
-singleList.display();
+//var singleList = new SingleLinkedList();
+//singleList.insert("hello", "head");
+//singleList.insert("world", "hello");
+//singleList.insert("dom", "world");
+//singleList.display();
+//singleList.remove("world");
+//singleList.display();
+
+
+function DoubleLinkedList () {
+    var me = this;
+    me.head = new Node("head");
+
+    me.find = function (item) {
+        var currentNode = me.head;
+        while (currentNode.data != item){
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    },
+
+        me.insert = function (newData, item) {
+            var newNode = new Node(newData);
+            var current = this.find(item);
+            newNode.next = current.next;
+            //only new line.
+            newNode.previous =current;
+            current.next = newNode;
+        },
+
+        me.display = function () {
+            var currentNode = me.head;
+            while (!(currentNode.next === null)) {
+                console.log(currentNode.next.data);
+                currentNode = currentNode.next;
+            }
+        },
+
+        me.findPrevious = function (item) {
+            var currentNode = this.head;
+            while(!(currentNode.next === null) &&
+            (currentNode.next.data != item)) {
+                currentNode = currentNode.next;
+            }
+            return currentNode;
+        },
+
+        me.remove = function (item) {
+            var currentNode = me.find(item);
+            if (!(currentNode.next === null)) {
+                currentNode.previous.next = currentNode.next;
+                currentNode.next.previous = currentNode.previous;
+                currentNode.next = null;
+                currentNode.previous = null;
+            }
+        }
+};
+
+var doubleLinkedList = new DoubleLinkedList();
+doubleLinkedList.insert("hello", "head");
+doubleLinkedList.insert("world", "hello");
+doubleLinkedList.insert("dom", "world");
+doubleLinkedList.display();
+doubleLinkedList.remove("world");
+doubleLinkedList.display();
 
 
 
