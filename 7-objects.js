ec77a48b86bdbfa59edf45c639e731ef7b22b44a
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-04 11:46:09 +0100

    DoubleLinkedList now inherits from single linked list object. might need more refactoring.

--- 7-objects.js
+++ 7-objects.js
@@ -61,66 +61,42 @@ function SingleLinkedList () {
 //singleList.display();
 //singleList.remove("world");
 //singleList.display();
+//console.log("");
 
 
 function DoubleLinkedList () {
     var me = this;
     me.head = new Node("head");
 
-    me.find = function (item) {
-        var currentNode = me.head;
-        while (currentNode.data != item){
-            currentNode = currentNode.next;
-        }
-        return currentNode;
+    me.insert = function (newData, item) {
+        var newNode = new Node(newData);
+        var current = this.find(item);
+        newNode.next = current.next;
+        //only new line.
+        newNode.previous =current;
+        current.next = newNode;
     },
 
-        me.insert = function (newData, item) {
-            var newNode = new Node(newData);
-            var current = this.find(item);
-            newNode.next = current.next;
-            //only new line.
-            newNode.previous =current;
-            current.next = newNode;
-        },
-
-        me.display = function () {
-            var currentNode = me.head;
-            while (!(currentNode.next === null)) {
-                console.log(currentNode.next.data);
-                currentNode = currentNode.next;
-            }
-        },
-
-        me.findPrevious = function (item) {
-            var currentNode = this.head;
-            while(!(currentNode.next === null) &&
-            (currentNode.next.data != item)) {
-                currentNode = currentNode.next;
-            }
-            return currentNode;
-        },
-
-        me.remove = function (item) {
-            var currentNode = me.find(item);
-            if (!(currentNode.next === null)) {
-                currentNode.previous.next = currentNode.next;
-                currentNode.next.previous = currentNode.previous;
-                currentNode.next = null;
-                currentNode.previous = null;
-            }
+    me.remove = function (item) {
+        var currentNode = me.find(item);
+        if (!(currentNode.next === null)) {
+            currentNode.previous.next = currentNode.next;
+            currentNode.next.previous = currentNode.previous;
+            currentNode.next = null;
+            currentNode.previous = null;
         }
+    }
 };
 
+DoubleLinkedList.prototype = new SingleLinkedList();
 var doubleLinkedList = new DoubleLinkedList();
 doubleLinkedList.insert("hello", "head");
 doubleLinkedList.insert("world", "hello");
 doubleLinkedList.insert("dom", "world");
 doubleLinkedList.display();
+console.log();
 doubleLinkedList.remove("world");
 doubleLinkedList.display();
-
-
 
 
 
